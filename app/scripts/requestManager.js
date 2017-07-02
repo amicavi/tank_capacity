ac92d5c92614731981dbbf7ce6e2b228520cde48
amicavi <dev.patsy.iv@gmail.com>
2017-07-01 20:40:02 -0500

    added, get makes and models for the select inputs

--- app/scripts/requestManager.js
+++ app/scripts/requestManager.js
@@ -9,7 +9,43 @@ function requestManager () {
       	  "Authorization": "Basic " + btoa("tank_capacity:JT^0s#^X56Bm*M")
     	  },
         success:function(data){
-    	    console.log("success");
+    	    console.log("getCarYears success");
+        	console.log(data);
+          cb(data);
+        },
+        error: function(e){
+        	console.log(e);
+        }
+      });
+    };
+
+    self.getCarMakes = function(year,cb) {
+      $.ajax({
+      	url: 'http://mvp.clonemirefaccion.com/vechicle/fleet/api/makes?access_token=eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiOGYwZWIzZjU5In0.8MNTm8SwVmkeLCR5omERTXzaqtPVnByg0wLcnOYHvBY&year=' + year,
+        type: "GET",
+        headers: {
+      	  "Authorization": "Basic " + btoa("tank_capacity:JT^0s#^X56Bm*M")
+    	  },
+        success:function(data){
+    	    console.log("getCarMakes success");
+        	console.log(data);
+          cb(data);
+        },
+        error: function(e){
+        	console.log(e);
+        }
+      });
+    };
+
+    self.getCarModels = function(year, make, cb) {
+      $.ajax({
+      	url: 'http://mvp.clonemirefaccion.com/vechicle/fleet/api/models?access_token=eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiOGYwZWIzZjU5In0.8MNTm8SwVmkeLCR5omERTXzaqtPVnByg0wLcnOYHvBY&year=' + year + '&make=' + make,
+        type: "GET",
+        headers: {
+      	  "Authorization": "Basic " + btoa("tank_capacity:JT^0s#^X56Bm*M")
+    	  },
+        success:function(data){
+    	    console.log("getCarMakes success");
         	console.log(data);
           cb(data);
         },
--- app/scripts/tankCapacityViewModel.js
+++ app/scripts/tankCapacityViewModel.js
@@ -1,17 +1,47 @@
 function tankCapacityViewModel() {
   var self = this;
 
-  this.year  = ko.observable();
-  this.brand = ko.observable(null);
-  this.model = ko.observable(null);
-  this.engine = ko.observable(null);
+  this.selectedYear   = ko.observable();
+  this.selectedBrand  = ko.observable();
+  this.selectedModel  = ko.observable();
+  this.selectedEngine = ko.observable();
 
-  this.years = ko.observableArray([]);
+  this.years  = ko.observableArray([]);
+  this.brands = ko.observableArray([]);
+  this.models = ko.observableArray([]);
 
   requestManager().getCarYears(function (years) {
-    self.years(self.years().concat(years));
+    console.log("1");
+    self.years(years);
+    self.getBrands();
   });
+
+
+  this.getBrands = function() {
+    self.selectedYear.subscribe(function (year) {
+      requestManager().getCarMakes(year, function (makes) {
+        self.brands(makes);
+        self.getModels(year);
+       });
+    });
+  };
+
+  this.getModels = function(year) {
+    self.selectedBrand.subscribe(function (brand) {
+      requestManager().getCarModels(year, brand, function (models) {
+        self.models(models);
+      });
+    });
+  };
+
+  // this.brand = ko.pureComputed(function() {
+  //     requestManager().getCarYears(this.year, function (years) {
+  //       self.years(self.years().concat(years));
+  //     });
+  //   });
+
 };
+
 
 
 $(function() {
